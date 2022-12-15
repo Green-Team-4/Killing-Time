5b7356da71570326355721b19151c65fe67c9534
jhyungkim <dnlwmxj135@gmail.com>
2022-12-15 18:03:29 +0900

    검색기능 수정

--- Killing-Time/src/views/search/search/Movie.js
+++ Killing-Time/src/views/search/search/Movie.js
@@ -19,19 +19,21 @@ const setVoteClass = (vote) => {
 const Movie = ({id, title, poster_path, release_date, vote_average }) => (
     <div className="movie">
         <p className='moviee'>
-        <Link to="/movieDetail" state={{ id: id }}>  
-        <img style={{ width:300, height: 300}}
-            src={
-                poster_path 
-                ? IMG_API + poster_path
-                // 영화 포스터 깨질경우 아래 이미지로 대처
-                : "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=740&q=80"
-                }
-                alt={title} />
-                </Link>
+                <Link to="/moviePage/movieDetail" state={{ id: id }}>  
+                        <div style={{width:290}}>
+                        <img style={{ width:270, height: 300}}
+                        src={
+                            poster_path 
+                            ? IMG_API + poster_path
+                            // 영화 포스터 깨질경우 아래 이미지로 대처
+                            : "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=740&q=80"
+                            }
+                            alt={title} />
+                        </div>
+                </Link>            
                 <div className="movie-info">
                 &nbsp;
-                    <Link to="/movieDetail" state={{ id: id }}><h5>{title}</h5></Link>
+                    <Link to="/moviePage/movieDetail" state={{ id: id }}><h5>{title}</h5></Link>
                     <br />
                     <span className={
                             `tag ${setVoteClass(release_date)}`
