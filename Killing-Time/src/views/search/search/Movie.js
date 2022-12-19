0e9ebc35d06ce04c9d2b6d5e95684609230b12f6
jhyungkim <dnlwmxj135@gmail.com>
2022-12-19 11:37:47 +0900

    search 수정

--- Killing-Time/src/views/search/search/Movie.js
+++ Killing-Time/src/views/search/search/Movie.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { CCard, CCardImage, CCardTitle, CCardText, CCardBody, CCardHeader } from '@coreui/react';
+
 
  const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 //가져올 목록
 const Movie = ({id, title, poster_path, release_date, vote_average }) => (
-    <div className="movie">
-        <p className='moviee'>
+    <CCard className='mb-3 border-dark' textColor='dark' style={{borderRadius:10, borderWidth:1, margin:7, width:222}}>
                 <Link to="/moviePage/movieDetail" state={{ id: id }}>  
-                        <div style={{width:240}}>
-                        <img style={{ width:220, height: 330, textAlign:'center',borderRadius:20}}
+                        <CCardImage style={{width:220, height: 300, textAlign:'center', borderRadius:10}}
                         src={
                             poster_path 
                             ? IMG_API + poster_path
@@ -17,26 +17,22 @@ const Movie = ({id, title, poster_path, release_date, vote_average }) => (
                             : "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=740&q=80"
                             }
                             alt={title} />
-                        </div>
-                </Link>            
-                <div style={{fontSize:15, color:'black'}} className="movie-info">
+                </Link>        
+                <CCardHeader style={{backgroundColor:'#F0F8FF', fontSize:15, color:'black'}} className="movie-info">
                 &nbsp;
-                    <Link style={{fontSize:20, textDecoration:'none', color:'black'}} to="/moviePage/movieDetail" state={{ id: id }}>
-                        <h5 style={{fontWeight:'bold'}}>{title}</h5>
+                    <Link style={{fontSize:18, textDecoration:'none', color:'black'}} to="/moviePage/movieDetail" state={{ id: id }}>
+                        <span style={{fontWeight:'bold'}}>{title}</span>
                     </Link>
-                    <br />
-                    </div>
-                    <div style={{display:'inline-block'}}>
-                        <span>
+                    </CCardHeader>
+                    <CCardBody style={{display:'inline-block', backgroundColor:'#F5FFFA'}}>
+                        <span style={{fontWeight:'bold'}}> 
                         개봉일 :  {release_date}
                         <br />
-                        평점 :  {vote_average}
+                        회원 점수 :  {vote_average}
                         </span>
-                    </div>
-                <br />
-            <br />
-        </p>
-    </div>
+                    </CCardBody>
+                    <br />
+    </CCard>
 );
 
 export default Movie;
