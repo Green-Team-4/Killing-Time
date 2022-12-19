cbfc55afcfc273d8055b9b4b327d24d6f8e0ec68
iwinegev <jornojovanahw@gmail.com>
2022-12-19 16:23:35 +0900

    123

--- Killing-Time/src/views/person/PersonDetailCast.js
+++ Killing-Time/src/views/person/PersonDetailCast.js
@@ -30,6 +30,9 @@ const PersonDetailCast = ({id}) => {
     const [tvCastResults, setTvCastResults] = useState(null);
     const [castPage, setCastPage] = useState(null);
 
+    const [loadCount, setLoadCount] = useState(10);
+    const [visible, setVisible] = useState(1);
+
     // useEffect : mount(초기화), update(상태변화) 이벤트 처리기 등록
     useEffect(() => {
         const loadCastList = async (e) => {
@@ -70,26 +73,50 @@ const PersonDetailCast = ({id}) => {
                         fontSize:"24px"
                     }}>출연작</span>
                     <Button
-                        onClick={() => setCastPage("movie")} disabled={castPage === "movie"}
+                        onClick={() => {setCastPage("movie"); setLoadCount(10); setVisible(1);}} disabled={castPage === "movie"}
                     >영화</Button>
                     <span style={{
                         margin:"10px"
                     }}>|</span>
                     <Button
-                        onClick={() => setCastPage("tv")} disabled={castPage === "tv"}
+                        onClick={() => {setCastPage("tv"); setLoadCount(10); setVisible(1);}} disabled={castPage === "tv"}
                     >TV시리즈</Button>
                     <hr/><br/>
                     {
                         castPage === "movie"
                         ?
                         movieCastResults.map( (movieCastResult, idx) => {
-                            return (<PersonDetailCastItem key={ idx } castResult={ movieCastResult } MovieOrTv="movie" />);
+                            if (idx < loadCount) {
+                                return (<PersonDetailCastItem key={ idx } castResult={ movieCastResult } MovieOrTv="movie" />);
+                            } else {
+                                return "";
+                            }
                         })
+                        
                         :
                         tvCastResults.map( (tvCastResult, idx) => {
+                            if (idx < loadCount) {
                             return (<PersonDetailCastItem key={ idx } castResult={ tvCastResult } MovieOrTv="tv" />);
+                            } else {
+                                return "";
+                            }
                         })
                     }
+                    <div style={{textAlign:"center", marginBottom:"50px"}}>
+                    {
+                        visible === 1
+                        ?
+                        <Button id="allview" onClick={() => 
+                            {
+                                setLoadCount(65535);
+                                setVisible(0);
+                            }
+                        }>전부 보기
+                        </Button>
+                        :
+                        <></>
+                    }
+                    </div>
                 </CCardBody>
             </CCard>
         </CCol>
