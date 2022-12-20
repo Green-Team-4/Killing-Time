7738d261f828d72c7ed8fd46fac5d140125eb3ef
iwinegev <jornojovanahw@gmail.com>
2022-12-20 17:18:19 +0900

    12313123

--- Killing-Time/src/views/person/PersonList.js
+++ Killing-Time/src/views/person/PersonList.js
@@ -21,7 +21,6 @@ const PersonList = (props) => {
 
     // useEffect : mount(초기화), update(상태변화) 이벤트 처리기 등록
     useEffect( () => {
-        debugger;
         if (searchKeyword === null | searchKeyword === "") {
             const loadPersonList = async (e) => {
                 const url = 
