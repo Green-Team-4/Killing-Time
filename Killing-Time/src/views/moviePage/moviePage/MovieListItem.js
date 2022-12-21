7f9158cfcd9c3f553509ed7bfe95df9a1053092d
urdoon <mun7646@gmail.com>
2022-12-21 12:22:41 +0900

    영화 목록 수정중

--- Killing-Time/src/views/moviePage/moviePage/MovieListItem.js
+++ Killing-Time/src/views/moviePage/moviePage/MovieListItem.js
@@ -7,24 +7,36 @@ const MovieItemBlock = styled.div`
 `;
 
 const MovieListItem = ({ result }) => {
-  const { id, poster_path, title } = result;
+  const { id, poster_path, title, vote_average, release_date } = result;
   const img_url = `https://www.themoviedb.org/t/p/w220_and_h330_face${poster_path}`;
-
+  const rate = vote_average * 10;
   return (
     <>
       <MovieItemBlock>
         
+      <Link to="/moviePage/movieDetail" state={{ id: id }} style={{textDecoration: "none", color: "black",}}>
         <div style={{ width: 220 }}>
-          <Link to="/moviePage/movieDetail" state={{ id: id }}>
+          
             <img src={img_url} alt="movie thumbnail" style={{ borderRadius: 10 }} />
-          </Link>
+            
+          
         </div>
-        <div style={{fontSize: 14, textDecoration: "none", color: "black",width:220, height:25, overflow: "hidden", textOverflow: "ellipsis", textAlign: "center"}}>
-          <Link to="/moviePage/movieDetail" state={{ id: id }} style={{textDecoration: "none", color: "black",}}>
-            {title}
-          </Link>
+        <div style={{fontSize: 14, textDecoration: "none", color: "black", width:220, overflow: "hidden", textOverflow: "ellipsis", textAlign: "center"}}>
+          
+            <div>{title}</div>
+            <div>
+              <span>
+              {
+                              rate > 66 ? <strong style={{ color: "#369F36" }}>● {rate}%</strong> :
+                              rate > 33 ? <strong style={{ color: "#FFAF0A" }}>● {rate}%</strong> :
+                              <strong style={{ color: "#EB0000" }}>● {rate}%</strong>
+                            }
+              </span>
+              {release_date}
+            </div>
+          
         </div>
-        
+        </Link>
       </MovieItemBlock>
     </>
   );
