3fc8170cc5bb57636941e8d70a3f1ea6566dfb40
sbh1893 <sbh1893@gmail.com>
2022-12-22 12:29:29 +0900

    update boxoffice chart

--- Killing-Time/src/home/BoxOfficeChart.js
+++ Killing-Time/src/home/BoxOfficeChart.js
@@ -21,22 +21,54 @@ const BoxOfficeChart = (props) => {
     const [ boxOfficeChart, setBoxOfficeChart ] = useState(null);
 
     useEffect( () => {
+        const boxOfficeChartLoad = async (e) => {
 
-    const boxOfficeChartLoad = async (e) => {
+        const apikey = "9ec0af98ce7854a7d4109bd050f4e2cf";
 
+        const dt = new Date();
+        const results = [];
+        for (let i = 1; i < 8; i++) {
+          dt.setDate(dt.getDate() - 1);
+
+          const targetDt = `${dt.getFullYear()}${dt.getMonth() + 1}${dt.getDate()}`;
+          const itemPerPage = 5;
+          const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${apikey}&targetDt=${targetDt}&itemPerPage=${itemPerPage}`
+          const response = await axios.get(url);
+          results.push(response.data.boxOfficeResult.dailyBoxOfficeList);
+        }
+
+        const boxOfficeData = results[0].map( (movie, idx) => {
+        
+          const boxOfficeByMovie = {
+            movieCd : movie.movieCd,
+            movieNm : movie.movieNm
+          };
+          const boxOffice = [movie.audiCnt];
+          for (let j = 1; j < results.length; j++) {
+            let found = false;
+            for (let k = 0; k < results[j].length; k++) {
+              if (movie.movieCd === results[j][k].movieCd) {
+                boxOffice.push(results[j][k].audiCnt);
+                found = true;
+                break;
+              }
+            }
+            if (!found) {
+              boxOffice.push(0);
+            }
+          }
+          boxOfficeByMovie.audiCntList = boxOffice;
+          return boxOfficeByMovie;
+          //boxOfficeData.push(boxOfficeByMovie);
+        });
+
+        setBoxOfficeChart(boxOfficeData);
+        
+      }
+      boxOfficeChartLoad();
+    },[]);
     
 
-
-    const apikey = "9ec0af98ce7854a7d4109bd050f4e2cf";
-    const targetDt = `${year}${month}${date2}`;
-    const itemPerPage = 5;
-    const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${apikey}&targetDt=${targetDt}&itemPerPage=${itemPerPage}`
-    const response = await axios.get(url);
-    setBoxOfficeChart(response.data.boxOfficeResult);
-  }
-  boxOfficeChartLoad();
-},[]);
-    
 const [activeIndex, setActiveIndex]=useState(0);
 
 const tabClickHandler=(index)=>{
@@ -44,36 +76,46 @@ const tabClickHandler=(index)=>{
 };
 
 
-
-const tabContArr=[
     
-    {
-        tabTitle:(
-            boxOfficeChart ? 
-                boxOfficeChart.dailyBoxOfficeList.map((dailyBoxOfficeLists) => { 
-                   var i = 0;
-                    for (i = 0; i <= 5; i++){
-                    
-                    return (
-               
-            <CTooltip 
-                content="일일 박스 오피스를 확인하세요."
-                placement="bottom">
-            <CButton color='light' style={{ fontSize: 18}} className={activeIndex===i ? "is-active" : ""} onClick={()=>tabClickHandler(i)}>                
-            <small>{dailyBoxOfficeLists.movieNm} {i}</small>
-                </CButton>
-            </CTooltip>
-            )}            
-                })   
-                 :
-                 "aaa"
+// const tabContArr=[
+    
+//     {
+//         tabTitle:(
+          
+//             <CTooltip 
+//                 content="일일 박스 오피스를 확인하세요."
+//                 placement="bottom">
+//             <CButton color='light' style={{ fontSize: 18}} className={activeIndex=== 0 ? "is-active" : ""} onClick={()=>tabClickHandler(0)}>                
+//             <small> {boxOfficeChart.dailyBoxOfficeList[0].movieNm}</small>
+//                 </CButton>
+//             </CTooltip>
+            
                  
-        ),
-        tabCont:(
-            <div></div>
-        )
-    }
-];
+//         ),
+//         tabCont:(
+
+//          <div> <CCardBody>
+//             <CChartLine
+//              data={{
+//                labels: [`${month}-${date1}`, `${month}-${date2}`, `${month}-${date3}`, `${month}-${date4}`, `${month}-${date5}`, `${month}-${date6}`, `${month}-${date7}`,`${month}-${date8}`],
+//                datasets: [                 
+//                  {
+//                    label: 'My Second dataset',
+//                    backgroundColor: 'rgba(151, 187, 205, 0.2)',
+//                    borderColor: 'rgba(151, 187, 205, 1)',
+//                    pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+//                    pointBorderColor: '#fff',
+//                    data: [22,33,44,55,66,77,88],
+//                  },
+//                ],
+//              }}
+//             />
+//             </CCardBody> 
+//             </div>
+
+//         )
+//     }
+// ];
 
 // return (
 //     <div>
@@ -93,35 +135,31 @@ const tabContArr=[
         <>
         <CCol xs={10} style={{margin: "auto"}}>
         <CCard className="mb-4">
-          <CCardHeader><strong>영화별 일일 관객수</strong>  <div className="tabs">{tabContArr.map((section, index)=>{
-            return section.tabTitle
-        })}
-      </div> </CCardHeader>
-          <CCardBody>
-            <CChartLine
-              data={{
-                labels: [`${month}-${date1}`, `${month}-${date2}`, `${month}-${date3}`, `${month}-${date4}`, `${month}-${date5}`, `${month}-${date6}`, `${month}-${date7}`,`${month}-${date8}`],
-                datasets: [
-                  {
-                    label: 'My First dataset',
-                    backgroundColor: 'rgba(220, 220, 220, 0.2)',
-                    borderColor: 'rgba(220, 220, 220, 1)',
-                    pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-                    pointBorderColor: '#fff',
-                    data: [6,7,33,66,33,55,88],
-                  },
-                  {
-                    label: 'My Second dataset',
-                    backgroundColor: 'rgba(151, 187, 205, 0.2)',
-                    borderColor: 'rgba(151, 187, 205, 1)',
-                    pointBackgroundColor: 'rgba(151, 187, 205, 1)',
-                    pointBorderColor: '#fff',
-                    data: [22,33,44,55,66,77,88],
-                  },
-                ],
-              }}
+          <CCardHeader><strong>영화별 일일 관객수</strong>  <div className="tabs">
+            {/* {tabContArr.map((section, index)=>{
+              return section.tabTitle
+        })} */}
+      </div></CCardHeader>
+        <div>
+           {/* {tabContArr[activeIndex].tabCont} */}
+           <CCardBody>
+             <CChartLine
+             data={{
+               labels: [`${month}-${date2}`, `${month}-${date3}`, `${month}-${date4}`, `${month}-${date5}`, `${month}-${date6}`, `${month}-${date7}`, `${month}-${date8}`],
+               datasets: boxOfficeChart?.map( (boxOfficeByMovie, idx) => {
+                return {
+                  label: boxOfficeByMovie.movieNm,
+                  backgroundColor: 'rgba(151, 187, 205, 0.2)',
+                  borderColor: 'rgba(151, 187, 205, 1)',
+                  pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+                  pointBorderColor: '#fff',
+                  data: boxOfficeByMovie.audiCntList
+                }
+               })
+             }}
             />
-          </CCardBody>
+            </CCardBody>
+        </div>
         </CCard>
       </CCol>
         </>
@@ -129,3 +167,5 @@ const tabContArr=[
 };
 
 export default BoxOfficeChart;
+
+
