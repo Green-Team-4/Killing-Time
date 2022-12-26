56a5512bf7afc971e4929c7da2ebd8869e1e3d73
iwinegev <jornojovanahw@gmail.com>
2022-12-26 16:47:34 +0900

    12313@!

--- Killing-Time/src/views/person/PersonDetail.js
+++ Killing-Time/src/views/person/PersonDetail.js
@@ -2,16 +2,12 @@ import { useLocation } from 'react-router-dom';
 import PersonDetailCast from './PersonDetailCast';
 import PersonDetailInfo from './PersonDetailInfo';
 
-
 const PersonDetail = (props) => {
 
     const location = useLocation();
     const id = location.state.id;
-    const page = location.state.page;
-    const historys = location.state.history;
-
-    console.log('page, history: ', page, historys);
-
+    // const page = location.state.page;
+    // console.log('page: ', page);
 
     return (
         <div>
